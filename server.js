ec888b50ba741d5432c9e1834ab84468bf0587f0
GilGuereque <gguereque326@gmail.com>
2024-01-07 19:48:17 -0800

    Setting up the Express app and Server connection

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
-function test (){
-    console.log("test");
-};
+// Set up Express app
+const express = require('express');
+const app = express();
+const PORT = 2121 || process.env.PORT;
+
+
+// Set up the Routes
+const mainRoutes = require('./routes/main');
+
+
+// Set up middleware for Express
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Use Routes
+app.use('/', mainRoutes);
+
+
+// Set up PORT connection
+app.listen(PORT, () => {
+    console.log(`The server is running on PORT ${PORT}`);
+});
