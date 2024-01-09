f873882ae84cc19b6d843d28248107e11db55ece
GilGuereque <gguereque326@gmail.com>
2024-01-08 19:40:14 -0800

    Updated server.js to include GET call to external API

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Set up Express app
 const express = require('express');
 const app = express();
+const request = require('request');
 const PORT = 2121 || process.env.PORT;
 
 
@@ -18,6 +19,29 @@ app.use(express.json());
 // Use Routes
 app.use('/', mainRoutes);
 
+
+// Set up GET request to Nutrition API
+app.get('/nutrition', (req, res) => {
+    const query = req.query.food;
+
+    request.get({
+        url: 'https://api.calorieninjas.com/v1/nutrition?query=' + query,
+        headers: {
+            'X-Api-Key': process.env.YOUR_API_KEY
+        },
+    }, (error, response, body) => {
+        if (error) {
+            console.error('Request failed:', error);
+            res.status(500).json({ error: 'Request to external API failed' }); // Send error message to client
+        } else if (response.statusCode !== 200) {
+            console.error('Error:', response.statusCode, body.toString('utf8'));
+            res.status(response.statusCode).json({ error: 'Request to external API failed' }); // Send error message to client
+        } else {
+            const nutritionData = JSON.parse(body);
+            res.json(nutritionData); // Send data to client
+        }
+    });
+});
 
 // Set up PORT connection
 app.listen(PORT, () => {
